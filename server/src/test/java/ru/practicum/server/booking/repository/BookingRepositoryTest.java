37a5de825819b5a3befa580ee0e0726cf3f5be97
evgenii <gulllak@yandex.ru>
2024-07-15 22:52:30 +0300

    fix: добавили профиль test

--- server/src/test/java/ru/practicum/server/booking/repository/BookingRepositoryTest.java
+++ server/src/test/java/ru/practicum/server/booking/repository/BookingRepositoryTest.java
@@ -3,10 +3,13 @@ package ru.practicum.server.booking.repository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
+import ru.practicum.server.BaseTest;
 import ru.practicum.server.booking.entity.BookingEntity;
 import ru.practicum.server.booking.model.BookingStatus;
 import ru.practicum.server.item.entity.ItemEntity;
@@ -18,6 +21,8 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class BookingRepositoryTest {
     @Autowired
     TestEntityManager entityManager;
