a1a506300cae0da34dd04534020cf9d6fbfb1d6e
evgenii <gulllak@yandex.ru>
2024-07-31 09:14:57 +0300

    Добавили url Logstash в Server application.properties

--- server/src/main/java/ru/practicum/server/user/controller/UserController.java
+++ server/src/main/java/ru/practicum/server/user/controller/UserController.java
@@ -2,6 +2,7 @@ package ru.practicum.server.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jboss.logging.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
